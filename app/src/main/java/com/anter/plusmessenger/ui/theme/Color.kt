8825950a2cc0f5c45fa51bdc5fff0205036fb3e0
smart2ye <smart2ye@gmail.com>
2026-09-15 16:27:41 +0300

    fix: add missing Color.kt and Type.kt (theme files)

---
+++ app/src/main/java/com/anter/plusmessenger/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.anter.plusmessenger.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AnterBlue = Color(0xFF1877F2)
+val AnterBlueDark = Color(0xFF0B5ED7)
+val AnterBlueLight = Color(0xFF4F9BFF)
+val AnterBg = Color(0xFFFFFFFF)
+val AnterSurface = Color(0xFFF0F2F5)
+val AnterText = Color(0xFF050505)
+val AnterTextMuted = Color(0xFF65676B)
+val AnterDivider = Color(0xFFE5E7EB)
+val AnterError = Color(0xFFE53935)
